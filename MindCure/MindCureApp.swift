ab9429fbaf16e35bf5a7badeec99f42d10063c12
kurortn1y <lastmandanc1ng@icloud.com>
2025-08-19 16:26:44 +0300

    Initial Commit

---
+++ MindCure/MindCureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindCureApp.swift
+//  MindCure
+//
+//  Created by kurortn1y on 19.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct MindCureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
